a9a43187a0a0069c74a735bf9fd62b704b13a92e
huynd <nguyendanghuy071195@gmail.com>
2023-09-20 21:56:29 +0700

    adding connect mysql with mysql2

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { engine } = require('express-handlebars')
 const bodyParser = require('body-parser')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+const db = require('./helpers/database')
 
 const app = express()
 
@@ -27,58 +28,15 @@ app.use('', (req, res, next) => {
 	res.status(404).send('<h1>Page not found!</h1>')
 })
 
-// const server = http.createServer((req, res) => {
-// 	console.log('req url', req.url)
-// 	console.log('req header', req.headers)
-// 	console.log('req method', req.method)
-// 	// process.exit()
-// 	res.writeHead(200, { 'Content-Type': 'text/html' })
-
-// 	if (req.url === '/products') {
-// 		// return res.write(
-// 		// 	`<html>
-// 		//     <head>
-// 		//       <title>Product Page</title>
-// 		//     </head>
-// 		//     <body>
-// 		//       <h1>Product Content</h1>
-// 		//     </body>
-// 		//   </html>`
-// 		// )
-// 		// res.writeHead(302)
-// 		// res.write
-// 	}
-
-// 	if (req.url === '/add-product' && req.method === 'POST') {
-// 		let body = []
-// 		req.on('data', (chunk) => {
-// 			console.log('chunk', chunk)
-// 			body.push(chunk)
-// 		})
-// 		req.on('end', () => {
-// 			body = Buffer.concat(body).toString()
-// 			// at this point, `body` has the entire request body stored in it as a string
-// 			console.log('body', body)
-// 			console.log('adsasd')
-// 			console.log('12121')
-// 		})
-// 		return res.end()
-// 	}
-// 	res.write(
-// 		`<html>
-//       <head>
-//         <title>Hello NodeJS Course</title>
-//       </head>
-//       <body>
-//         <h1>Content Response From Home</h1>
-//         <form action='/add-product' method='POST'>
-//           <input type='text' name='name' />
-//           <button type='submit'>Submit</button>
-//         </form>
-//       </body>
-//     </html>`
-// 	)
-// })
+db.execute('SELECT * FROM `products`')
+	.then((result) => {
+		console.log('Fetched!!!')
+		const [products, fields] = result
+		console.log('products', products)
+	})
+	.catch((err) => {
+		console.log(err)
+	})
 
 app.listen(3000)
 
